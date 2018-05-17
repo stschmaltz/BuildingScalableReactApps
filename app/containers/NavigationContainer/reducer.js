cd99d403e35ad09963361cd0e5ab139e03232e24
Shane Schmaltz <stschamltz@gmail.com>
2018-05-16 19:48:09 -0600

    Add navigation component

---
+++ app/containers/NavigationContainer/reducer.js
@@ -0,0 +1,38 @@
+/*
+ *
+ * NavigationContainer reducer
+ *
+ */
+
+import { fromJS } from 'immutable';
+import {
+  DEFAULT_ACTION,
+} from './constants';
+
+const initialState = fromJS({
+  topics: [
+    {
+      name: 'libraries',
+      description: 'links to useful open source libraries',
+    },
+    {
+      name: 'apps',
+      description: 'links to new and exciting apps',
+    },
+    {
+      name: 'news',
+      description: 'links to programming related news articles',
+    },
+  ],
+});
+
+function navigationContainerReducer(state = initialState, action) {
+  switch (action.type) {
+    case DEFAULT_ACTION:
+      return state;
+    default:
+      return state;
+  }
+}
+
+export default navigationContainerReducer;
